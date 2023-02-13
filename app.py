c17f848cc9c80e32d920a84d9c3cb71e3665b56f
Artiom Nazarchuk <sel_it@tut.by>
2023-02-13 15:16:25 +0300

    Adding a buy button

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 import os
+import secrets
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 
+# from cloudipsp import Api, Checkout
+
+# api = Api(merchant_id=1396424,
+#           secret_key='test')
+# checkout = Checkout(api=api)
+# data = {
+#     "currency": "USD",
+#     "amount": 10000
+# }
+# url = checkout.url(data).get('checkout_url')
+
+
 UPLOAD_FOLDER = 'static/image'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
+secret_key = secrets.token_hex(16)
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
@@ -29,6 +45,7 @@ class Products(db.Model):
     created_on = db.Column(db.DateTime, default=datetime.utcnow)
     updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
     isActive = db.Column(db.Boolean, default=True)
+    image = db.Column(db.String(50), nullable=True, unique=False)
     category_id = db.Column(db.Integer(), db.ForeignKey('categories.id'))
 
     def __repr__(self):
@@ -51,27 +68,20 @@ def products():
     products = Products.query.order_by(Products.created_on.desc()).all()
     return render_template('products.html', data=products)
 
-@app.route('/uploads/<name>')
-def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-app.add_url_rule(
-    "/uploads/<name>", endpoint="download_file", build_only=True
-)
 @app.route('/create', methods=['POST', 'GET'])
 def create():
     if request.method == 'POST':
 
         if request.form.get('category_create'):
 
-            if 'image' not in request.files:
-                flash('No image part')
-                return redirect(request.url)
             file = request.files['image']
-            # If the user does not select a image, the browser submits an
-            # empty image without a filename.
+
             if file.filename == '':
-                flash('No selected image')
+                flash('Изображение не выбрано')
                 return redirect(request.url)
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
@@ -90,6 +100,15 @@ def create():
 
         if request.form.get('product'):
 
+            file = request.files['image']
+
+            if file.filename == '':
+                flash('Изображение не выбрано')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
             category = request.form['category']
             category_create = Categories.query.filter_by(category=category).first()
 
@@ -99,7 +118,7 @@ def create():
             isActive = bool(request.form.get('isActive'))
             category_id = category_create.id
             product_create = Products(product=product, price=price, description=description, isActive=isActive,
-                                      category_id=category_id)
+                                      category_id=category_id, image=file.filename)
             try:
                 db.session.add(product_create)
                 db.session.commit()
@@ -112,13 +131,6 @@ def create():
     else:
         categories = Categories.query.order_by(Categories.category).all()
         return render_template('create.html', data=categories)
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-
 
 @app.route('/about')
 def about():
